766eb7b48385403676a5b9faab189450ca7d7a4d
smriti <smritipandit785@gmail.com>
2022-08-09 17:51:58 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,4 @@ Route::get('/', function () {
 Route::get('/profile','ProfileController@index')->name('profile.index');
 Route::get('/profile/create','ProfileController@create')->name('profile.create');
 Route::post('/profile/store','ProfileController@store')->name('profile.store');
-Route::get('/profile/edit/{id}','ProfileController@edit')->name('profile.edit');
-Route::delete('/profile/delete/{id}','ProfileController@destroy')->name('profile.destroy');
-Route::put('profile/update/{id}','ProfileController@update')->name('profile.update');
+
